642b9a32ec0d37a55112e74edd920286a322ca19
Nesanesis <orionvega2343@gmail.com>
2026-04-19 16:48:05 +0300

    Add starting server

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Сервер запущен на порту 8080")
+	http.HandleFunc("/result", HandleGet)
+	http.ListenAndServe(":8080", nil)
+}
